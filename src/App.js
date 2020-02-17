f9da211e8f0dabdd7996323beca58f328e9bf0ef
michael-basweti <baswetima@gmail.com>
2020-02-17 12:56:39 +0300

    props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import React, { Component } from 'react';
 import './App.css'
+import Navbar from './components/layouts/Navbar';
+import PropTypes from 'prop-types';
 
 
 class App extends Component {
+  static defaultProps = {
+    title: 'Github Finder',
+    icon: 'fab fa-github'
+  }
+
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+  }
   render (){
-    const name = 'John Doe';
+    // const name = 'John Doe';
 
     // const foo = () => 'Bar'
     // function foo1 (){
     //   return 'Bar2'
     // }
-  const loading = true;
+  // const loading = true;
   // if(loading){
   //   return <h1>Loading....</h1>;
   // }
   return (
     <div className="App">
-      {loading ? <h1>Loading....</h1> : <h2>Hello { name }</h2>}
-      
+      <Navbar title = 'Github Finder' icon='fab fa-github'/>
+      <h1>Hello </h1>
     </div>
   );
   }
---
+++ src/components/layouts/Navbar.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import '../../App.css'
+
+class Navbar extends Component {
+    render() {
+        return (
+            <nav className="navbar bg-primary">
+                <h1><i className={this.props.icon}/> {this.props.title}</h1>
+            </nav>
+        )
+    }
+}
+
+export default Navbar
