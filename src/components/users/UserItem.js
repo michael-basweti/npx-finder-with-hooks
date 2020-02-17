bbfb67c3f491e973c14cf264ce309ef498f1d773
michael-basweti <baswetima@gmail.com>
2020-02-17 13:09:46 +0300

    create userItem class

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import '../../App.css'
+
+class UserItem extends Component {
+    render(){
+        return(
+            <h1>hello Nigga</h1>
+        )
+    }
+}
+
+export default UserItem;
