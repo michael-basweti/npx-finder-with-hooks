6d9f34f3f1edcaf9d060d2742aa365c6673a2d4b
michael-basweti <baswetima@gmail.com>
2020-02-18 14:52:00 +0300

    search users skeleton

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css'
 import Navbar from './components/layouts/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
@@ -27,9 +28,15 @@ class App extends Component {
     loading: false
   }
 
+  // search github users
+  searchUsers = (text) => {
+    console.log(text);
+    
+  }
+
   async componentDidMount(){
     this.setState({loading: true})
-     const res = await axios.get('https://api.github.com/users');
+     const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_ID}`);
      console.log(res);
      this.setState({users:res.data,loading:false})
   }
@@ -39,6 +46,7 @@ class App extends Component {
     <div className="App">
       <Navbar title = 'Github Finder' icon='fab fa-github'/>
       <div className="container">
+      <Search searchUsers={this.searchUsers}/>
         <Users loading = {this.state.loading} users={this.state.users}/>
       </div>
     </div>
